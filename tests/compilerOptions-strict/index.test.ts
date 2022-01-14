a08896e4a48bbb1faddb6eb56f04644e30da6c33
mrazauskas <72159681+mrazauskas@users.noreply.github.com>
2022-01-14 19:25:35 +0200

    tests: extend test suite

--- tests/compilerOptions-strict/index.test.ts
+++ tests/compilerOptions-strict/index.test.ts
@@ -2,3 +2,14 @@ import { expectType } from "../../";
 import aboveZero from ".";
 
 expectType<number>(aboveZero(1));
+
+declare const loggedInUsername: string;
+
+const users = [
+  { name: "Oby", age: 12 },
+  { name: "Heera", age: 32 },
+];
+
+const loggedInUser = users.find((u) => u.name === loggedInUsername);
+
+expectType<number>(loggedInUser.age);
